39bcc71aff31e894bb72211a8170b96bfedfd587
Carlos De Guzman <raremagic01@gmail.com>
2025-04-10 01:02:04 +0800

    initialize options and answer

--- quiz_maker_program.py
+++ quiz_maker_program.py
@@ -2,9 +2,19 @@
 import tkinter as tk
 from tkinter import messagebox, filedialog
 import os
+
 #initailize
+question_bank = []
 
 #set functions for saved questions
+def submit_question():
+    question = entry_question.get()
+    option_a = entry_option_a.get()
+    option_b = entry_option_b.get()
+    option_c = entry_option_c.get()
+    option_d = entry_option_d.get()
+    correct_answer = correct_option.get()
+    
     #set rules and conditions
 
 #set function for saving into a file
