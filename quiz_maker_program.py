c17b154ed31b2ebcf8850edc6d3911e8bbd1fbd1
Carlos De Guzman <raremagic01@gmail.com>
2025-04-09 19:24:18 +0800

    update

---
+++ quiz_maker_program.py
@@ -0,0 +1,19 @@
+#Start
+
+#import for GUI
+#initailize
+
+#set functions for saved questions
+    #set rules and conditions
+
+#set function for saving into a file
+
+#set a function for exit
+
+#create GUI window
+    #create labels and input fields
+    #create buttons
+
+#run program
+
+#End
